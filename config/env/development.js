ff11452cf0a976c5798343d38433daf09c732f56
Chris Troutner <chris.troutner@gmail.com>
2019-02-07 17:22:57 -0800

    Switching env config files around a bit

--- config/env/development.js
+++ config/env/development.js
@@ -1,13 +1,11 @@
 /*
-export default {
-  session: 'secret-boilerplate-token',
-  token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-dev'
-}
+  These are the environment settings for the DEVELOPMENT environment.
+  This is the environment run by default with `npm start` if KOA_ENV is not
+  specified.
 */
 
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/p2pvps-server-dev'
+  database: 'mongodb://localhost:27017/koa-server-dev'
 }
