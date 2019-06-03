adad1afbcc8ff522d72ce46581ef92ad993a2ae0
Chris Troutner <chris.troutner@gmail.com>
2019-06-02 20:25:47 -0700

    Removing commented out code

--- bin/server.js
+++ bin/server.js
@@ -34,7 +34,6 @@ async function startServer () {
   app.use(errorMiddleware())
 
   // Used to generate the docs.
-  // app.use(convert(mount('/docs', serve(`${process.cwd()}/docs`))))
   app.use(mount('/', serve(`${process.cwd()}/docs`)))
 
   // User Authentication
