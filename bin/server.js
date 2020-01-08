e8edbed9c3aabb394bded2410e66c9e415250872
Chris Troutner <chris.troutner@gmail.com>
2020-01-07 17:48:50 -0800

    fix(mongoose): Using useUnifiedTopology

--- bin/server.js
+++ bin/server.js
@@ -24,7 +24,13 @@ async function startServer () {
   // Connect to the Mongo Database.
   mongoose.Promise = global.Promise
   mongoose.set('useCreateIndex', true) // Stop deprecation warning.
-  await mongoose.connect(config.database, { useNewUrlParser: true })
+  await mongoose.connect(
+    config.database,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }
+  )
 
   // MIDDLEWARE START
 
